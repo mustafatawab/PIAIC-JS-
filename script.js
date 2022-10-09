2d7b849918d44973e4ea4b8eab4ec31d25b558e1
Mustafa Tawab <115402517+mustafatawab@users.noreply.github.com>
2022-10-09 07:49:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-var _name  = prompt('Enter Your name');
+//Excersises of javascript
+
+let _name  = prompt('Enter Your name');
 
 
-console.log('Hello ', _name)
+console.log('Hello ', _name);
+
+console.log('in the lower case the name is ' , _name.toLowerCase());
+
+console.log('In the upper case the name is ' , _name.toUpperCase());
+
+//Another Excercise
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let famousPersonName = 'Albert Einsteine';
+
+let quote = ' A person who never made a mistake  never tried anything';
+
+
+console.log(famousPersonName , ' once said , "', quote, ' ."');
+
+
+
+
+
+
 
 
 
